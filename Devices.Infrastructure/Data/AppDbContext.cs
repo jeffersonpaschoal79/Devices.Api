fbf284f99543f3f5ea6e16c37c56ee9e8e8ebd25
unknown <jefferson_paschoal@hotmail.com>
2025-12-01 11:16:26 -0300

    Updated Entity, dbContext and repository

---
+++ Devices.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Devices.Domain.Entities;
+
+namespace Devices.Infrastructure.Data;
+
+public sealed class AppDbContext : DbContext
+{
+    public DbSet<Device> Devices { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Device>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Name).IsRequired();
+            b.Property(x => x.Brand).IsRequired();
+            b.Property(x => x.State).IsRequired();
+            b.Property(x => x.CreatedAt).IsRequired();
+        });
+    }
+}
