3c65ce49b14cdae2923e56051588a5638f765794
Jefferson <jefferson_paschoal@hotmail.com>
2025-12-02 11:44:58 -0300

    Added Unit testing

---
+++ Devices.UnitTests/Domain/DeviceTests.cs
@@ -0,0 +1,68 @@
+﻿using Devices.Domain.Entities;
+using Devices.Domain.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace Devices.Api.Tests.Domain;
+
+public class DeviceTests
+{
+    [Fact]
+    public void Create_WithValidValues_ReturnsSuccessDevice()
+    {
+        var result = Device.Create("iPhone", "Apple", DeviceState.Available);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value!.Name.Should().Be("iPhone");
+        result.Value.Brand.Should().Be("Apple");
+        result.Value.State.Should().Be(DeviceState.Available);
+        result.Value.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Create_WithInvalidName_ReturnsFailure(string? name)
+    {
+        var result = Device.Create(name!, "Brand");
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Name is required");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Create_WithInvalidBrand_ReturnsFailure(string? brand)
+    {
+        var result = Device.Create("Name", brand!);
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Brand is required");
+    }
+
+    [Fact]
+    public void Update_WhenInUse_CannotChangeNameOrBrand()
+    {
+        var d = Device.Create("N", "B", DeviceState.InUse).Value!;
+        var res = d.Update("NewName", null, null);
+
+        res.IsSuccess.Should().BeFalse();
+        res.Error.Should().Contain("Cannot update Name or Brand when device is in use");
+    }
+
+    [Fact]
+    public void Update_PartialChange_UpdatesOnlyProvidedFields()
+    {
+        var d = Device.Create("N", "B", DeviceState.Available).Value!;
+        var res = d.Update("N2", null, DeviceState.Inactive);
+
+        res.IsSuccess.Should().BeTrue();
+        d.Name.Should().Be("N2");
+        d.Brand.Should().Be("B");
+        d.State.Should().Be(DeviceState.Inactive);
+    }
+}
---
+++ Devices.UnitTests/UseCases/GetDevicesUseCaseTests.cs
@@ -0,0 +1,69 @@
+﻿using Devices.Application.UseCases;
+using Devices.Domain.Entities;
+using Devices.Domain.Enums;
+using Devices.Infrastructure.Repositories;
+using FluentAssertions;
+using Moq;
+
+namespace Devices.Api.Tests.UseCases;
+
+public class GetDevicesUseCaseTests
+{
+    [Fact]
+    public async Task GetAllAsync_ReturnsSuccessWithDevices()
+    {
+        // Arrange
+        var sample = new List<Device>
+        {
+            Device.Create("N1","B1", DeviceState.Available).Value!,
+            Device.Create("N2","B2", DeviceState.InUse).Value!
+        };
+
+        var repoMock = new Mock<IDeviceRepository>();
+        repoMock.Setup(r => r.GetAllAsync(default)).ReturnsAsync(sample);
+
+        var uc = new GetDevicesUseCase(repoMock.Object);
+
+        // Act
+        var result = await uc.GetAllAsync();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(sample);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_NotFound_ReturnsFailure()
+    {
+        // Arrange
+        var repoMock = new Mock<IDeviceRepository>();
+        repoMock.Setup(r => r.GetByIdAsync(1, default)).ReturnsAsync((Device?)null);
+
+        var uc = new GetDevicesUseCase(repoMock.Object);
+
+        // Act
+        var result = await uc.GetByIdAsync(1);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Device not found");
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_Found_ReturnsSuccessAndDevice()
+    {
+        // Arrange
+        var device = Device.Create("N", "B", DeviceState.Available).Value!;
+        var repoMock = new Mock<IDeviceRepository>();
+        repoMock.Setup(r => r.GetByIdAsync(device.Id, default)).ReturnsAsync(device);
+
+        var uc = new GetDevicesUseCase(repoMock.Object);
+
+        // Act
+        var result = await uc.GetByIdAsync(device.Id);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(device);
+    }
+}
